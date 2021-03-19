25406f91692c1d9d51d812d5547bd5ae6e16b40f
John Wayne Carreon Beco <alu2017243@365.stucom.com>
2021-03-19 11:55:59 +0100

    #2 Still doing the login

--- src/graphics/MainFrame.java
+++ src/graphics/MainFrame.java
@@ -7,14 +7,21 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MainFrame extends JFrame {
 	
 	private JPanel contentPanel;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField textFieldUsernameField;
+	private JPasswordField passwordField;
 	
 	public MainFrame() {
+		this.onCreate();
+	}
+	
+	private void onCreate() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 610, 350);
 		setResizable(false);
@@ -22,23 +29,49 @@ public class MainFrame extends JFrame {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPanel);
 		contentPanel.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(231, 49, 108, 20);
-		contentPanel.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(231, 102, 108, 20);
-		contentPanel.add(textField_1);
-		textField_1.setColumns(10);
-		
+		this.createTextField();
+	}
+	
+	private void createTextField() {
+		textFieldUsernameField = new JTextField();
+		textFieldUsernameField.setBounds(231, 122, 108, 20);
+		contentPanel.add(textFieldUsernameField);
+		textFieldUsernameField.setColumns(10);
+		this.createLoginButton();
+	}
+	
+	private void createLoginButton() {
 		JButton btnNewButton = new JButton("LOGIN");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 			}
 		});
-		btnNewButton.setBounds(238, 147, 89, 23);
+		btnNewButton.setBounds(231, 203, 108, 20);
 		contentPanel.add(btnNewButton);
+		this.createPasswordField();
 	}
+	
+	private void createPasswordField() {
+		passwordField = new JPasswordField();
+		passwordField.setBounds(231, 153, 108, 20);
+		contentPanel.add(passwordField);
+		this.createLabel();
+	}
+	
+	private void createLabel() {
+		JLabel loginLabel = new JLabel("LOGIN");
+		loginLabel.setBounds(269, 94, 36, 17);
+		contentPanel.add(loginLabel);
+		
+		JLabel usernameLabel = new JLabel("Username: ");
+		usernameLabel.setBounds(159, 124, 62, 17);
+		contentPanel.add(usernameLabel);
+		
+		JLabel passwordLabel = new JLabel("Password: ");
+		passwordLabel.setBounds(162, 151, 69, 25);
+		contentPanel.add(passwordLabel);
+	}
+	
+	
 }
